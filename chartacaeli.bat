5964480b449678a3b3cd2b4479f000691d61c971
otabuzzman <iuergen.schuck@gmail.com>
2020-02-12 13:04:00 +0100

    Update help message of Linux/ Windows scripts

--- chartacaeli.bat
+++ chartacaeli.bat
@@ -1,11 +1,18 @@
 @echo off
-rem Charta Caeli launch utility. Store and exec in WEB-INF directory.
+rem Charta Caeli launch utility.
 rem
 
 set PATH=%ProgramFiles%\Cygwin\usr\x86_64-w64-mingw32\sys-root\mingw\bin;%PATH%
 
-rem if CLASSPATH is set assume propper values for JAVA_LIBRARY_PATH and PATH as well
+rem if CLASSPATH is set assume propper values for JAVA_LIBRARY_PATH, JAVA_LOGPROP_FILE and PATH as well
 if not defined CLASSPATH set JAVA_LIBRARY_PATH=lib
+if not defined CLASSPATH (
+	if exist lib\logging.properties (
+		set JAVA_LOGPROP_FILE=lib\logging.properties
+	) else (
+		set JAVA_LOGPROP_FILE=classes\logging.properties
+	)
+)
 if not defined CLASSPATH set PATH=lib;%PATH%
 if not defined CLASSPATH set CLASSPATH=classes;lib;lib/*
 set VIEWER=gswin64c.exe -dBATCH -dNOPAUSE -q -
@@ -16,6 +23,7 @@ set args=
 :parse
 if "%~1"=="/k" ( set keep=1 & goto parsenext )
 if "%~1"=="/v" ( set args=viewer=%VIEWER% & goto parsenext )
+if "%~1"=="/h" goto usage
 set param=%~1
 if "%param:~0,1%"=="/" goto usage
 goto parsestop
@@ -32,13 +40,25 @@ if "%keep%"=="0" (
 
 java -classpath %CLASSPATH% ^
 	-Djava.library.path=%JAVA_LIBRARY_PATH% ^
-	-Djava.util.logging.config.file=lib/logging.properties ^
+	-Djava.util.logging.config.file=%JAVA_LOGPROP_FILE% ^
 	org.chartacaeli.ChartaCaeli %args% %1 %2
 
 exit /b %errorlevel%
 
 :usage
-echo "chartacaeli.bat [ /k /v ] <cdefs> [ <prefs> ]"
-echo "  /k keep user preferences"
-echo "  /v spawn viewer process"
+echo chartacaeli.bat [ /k /v ] ^<cdefs^> [ ^<prefs^> ]
+echo.
+echo   /k keep user preferences
+echo   /v spawn viewer process
+echo.
+echo The script assumes to be executed in web/WEB-INF if CLASSPATH is unset.
+echo To run the script from another folder several variable are required:
+echo.
+echo   JAVA_LIBRARY_PATH - list (PATH format) of folders the JVM searches for
+echo                       shared objects.
+echo   JAVA_LOGPROP_FILE - path and file name of logging.properties.
+echo   PATH              - list of folders the dynamic link loader searches
+echo                       for nested objects (e.g. caa.dll -^> aaplus.dll).
+echo   CLASSPATH         - list of directories the JVM looks up class files
+echo                       and JARs.
 exit /b 1
--- chartacaeli.sh
+++ chartacaeli.sh
@@ -1,27 +1,48 @@
-# Charta Caeli launch utility. Store and exec in WEB-INF directory.
+# Charta Caeli launch utility.
 #
 
-# if CLASSPATH is set assume propper values for JAVA_LIBRARY_PATH and LD_LIBRARY_PATH as well
-test -z "$CLASSPATH" && { JAVA_LIBRARY_PATH=lib ; export LD_LIBRARY_PATH=lib:$LD_LIBRARY_PATH ; export CLASSPATH=classes:lib:lib/* ; }
+# if CLASSPATH is set assume propper values for JAVA_LIBRARY_PATH, JAVA_LOGPROP_FILE and LD_LIBRARY_PATH as well
+test -z "$CLASSPATH" && {
+	JAVA_LIBRARY_PATH=lib
+	test -s lib/logging.properties \
+	&& JAVA_LOGPROP_FILE=lib/logging.properties \
+	|| JAVA_LOGPROP_FILE=classes/logging.properties
+	export LD_LIBRARY_PATH=lib:$LD_LIBRARY_PATH
+	export CLASSPATH=classes:lib:lib/* ; }
 VIEWER=${GS:-gs}\ -dBATCH\ -dNOPAUSE\ -q\ -
 
 usage() {
 	echo "chartacaeli.sh [ -kv ] <cdefs> [ <prefs> ]"
+	echo ""
 	echo "  -k keep user preferences"
 	echo "  -v spawn viewer process"
+	echo ""
+	echo "The script assumes to be executed in web/WEB-INF if CLASSPATH is unset."
+	echo "To run the script from another folder several variable are required:"
+	echo ""
+	echo "  JAVA_LIBRARY_PATH - list (PATH format) of folders the JVM searches for"
+	echo "                      shared objects."
+	echo "  JAVA_LOGPROP_FILE - path and file name of logging.properties."
+	echo "  LD_LIBRARY_PATH   - list of folders the dynamic link loader searches"
+	echo "                      for nested objects (e.g. caa.dll -> aaplus.dll)."
+	echo "  CLASSPATH         - list of directories the JVM looks up class files"
+	echo "                      and JARs."
 	exit 1
 }
 
 keep=0
 args=""
 
-while getopts "kv" opt ; do
+while getopts "hkv" opt ; do
 	case $opt in
 		k)
 		keep=1
 		;;
 		v)
 		args="viewer=$VIEWER"
+		;;
+		h)
+		usage
 		;;
 		*)
 		usage
@@ -37,7 +58,7 @@ test $keep -eq 0 && {
 
 java -classpath $CLASSPATH \
 	-Djava.library.path=$JAVA_LIBRARY_PATH \
-	-Djava.util.logging.config.file=lib/logging.properties \
+	-Djava.util.logging.config.file=$JAVA_LOGPROP_FILE \
 	org.chartacaeli.ChartaCaeli $args $1 $2
 
 exit $?
